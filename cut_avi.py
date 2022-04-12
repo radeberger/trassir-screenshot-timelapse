e14ef8ef6a32edcd67a12d5349e4efc9928ec77b
radeberger <radeberger@list.ru>
2022-04-12 15:21:08 +0300

    Trassir-14-09-2021

--- cut_avi.py
+++ cut_avi.py
@@ -2,8 +2,14 @@ from moviepy.editor import *
 
 # loading video gfg
 clip = VideoFileClip("video-sutki.avi")
+print("duration = ", clip.duration)
+print("fps = ", clip.fps)
+width, height = clip.size
+
+print("width = ", width)
+print("height = ", height)
 # getting only first 5 seconds
-clip = clip.subclip(10, 100)
+#clip = clip.subclip(10, 100)
 # showing clip
 #clip.ipython_display(width = 720)
-clip.write_videofile("cutted.mp4")
+#clip.write_videofile("cutted.mp4")
